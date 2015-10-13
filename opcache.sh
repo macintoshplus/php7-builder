75a347aeab42e1b72109b2cb51c57929f192f041
macintoshplus <jean-baptiste@nahan.fr>
2015-10-13 07:03:22 +0200

    add opcache ext

---
+++ opcache.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo Build $0
+
+cd /sources/php-src/ext/opcache
+
+/opt/php7/bin/phpize
+
+./configure --prefix=/opt/php7 --path-php-config=/opt/php7/bin/php-config
+
+make
+
+make install
