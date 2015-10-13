77be914e3ae44fc75dc05164e616f9fca10a2f4a
Jean-Baptiste Nahan <jean-baptiste@nahan.fr>
2015-10-13 15:30:23 +0200

    fix loop

--- generate_config.sh
+++ generate_config.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-
+if [ -d /opt/php7/etc/modules ] ; then
+echo Dir exist !
+else
 mkdir /opt/php7/etc/modules
+fi;
 
-for f in /opt/php7/lib/php/extensions/no-debug-non-zts-20151012/*.so
+for f in /opt/php7/lib/php/extensions/no-debug-non-zts-20151012/*.so; do
 
    echo 'extensions='$f > /opt/php7/etc/modules/$f
 
