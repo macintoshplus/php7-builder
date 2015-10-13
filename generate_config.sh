b4cf885fa26882c18833af08492ea19d2aaa7d19
Jean-Baptiste Nahan <jean-baptiste@nahan.fr>
2015-10-13 15:31:33 +0200

    fix path

--- generate_config.sh
+++ generate_config.sh
@@ -6,7 +6,9 @@ else
 mkdir /opt/php7/etc/modules
 fi;
 
-for f in /opt/php7/lib/php/extensions/no-debug-non-zts-20151012/*.so; do
+cd /opt/php7/lib/php/extensions/no-debug-non-zts-20151012/
+
+for f in /*.so; do
 
    echo 'extensions='$f > /opt/php7/etc/modules/$f
 
